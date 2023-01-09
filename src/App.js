ceffcff566329c49aa5a8572e7e291798142ce72
DavidTeo <starrynightskyhoney@gmail.com>
2023-01-09 21:54:39 +0530

    added final code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Home, Dashboard, Register, Edit, Error, PrivateRoute } from './pages';
+import Navbar from './components/Navbar';
 function App() {
   return (
-    <div>
-      <h1>Hello Govind !!!</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/' exact>
+          <Home />
+        </Route>
+        <PrivateRoute path='/dashboard' exact>
+          <Dashboard />
+        </PrivateRoute>
+        <Route path='/register'>
+          <Register />
+        </Route>
+        <Route path='/edit/:id'>
+          <Edit />
+        </Route>
+
+        <Route path='*'>
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
